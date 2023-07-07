5e18952fa4744b3fadafd1789a32bba4d99d21a4
Peter Chen <peterchen2017125@gmail.com>
2023-07-07 15:09:08 +0800

    update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,10 +7,30 @@ const StyledFooter = styled.footer`
   padding-block: 40px;
 `;
 
+const Flex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const FooterColumn = styled.div`
+  width: calc(100% / 3);
+  @media (min-width: 769px){
+    width: 20%;
+  }
+`;
+
 const Footer = () => {
   return (
     <StyledFooter>
-      <Container></Container>
+      <Container>
+        <Flex>
+          <FooterColumn>Column 1</FooterColumn>
+          <FooterColumn>Column 2</FooterColumn>
+          <FooterColumn>Column 3</FooterColumn>
+          <FooterColumn>Column 4</FooterColumn>
+          <FooterColumn>Column 5</FooterColumn>
+        </Flex>
+      </Container>
     </StyledFooter>
   )
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,72 @@
 import React from "react";
 import Container from "../common/Container";
 import { styled } from "styled-components";
+import Logo from "../../images/logo.png";
+import { Input } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const StyledHeader = styled.header`
   background-color: #d1011c;
   width: 100vw;
-  height: 200px;
+  padding-block: 16px;
 `;
 
+const StyledHeaderSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+const Navigator = styled.div`
+  a {
+    margin-inline: 4px;
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const Toolbar = styled.div`
+  a {
+    margin-inline: 6px;
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Container>Header</Container>
+      <Container>
+        <StyledHeaderSection>
+          <Navigator>
+            <a href="#!">蝦皮購物</a>
+            <a href="#!">下載</a>
+            <a href="#!">追蹤我們</a>
+            <a href="#!">部落格</a>
+          </Navigator>
+          <Toolbar>
+            <a href="#!">通知</a>
+            <a href="#!">幫助中心</a>
+          </Toolbar>
+        </StyledHeaderSection>
+        <StyledHeaderSection>
+          <Link to="/">
+            <img src={Logo} alt="logo" height={48} />
+          </Link>
+          <Flex>
+            <Input.Search style={{ marginRight: 8 }} placeholder="在商城搜尋 " onSearch={(value) => console.log(value)} enterButton></Input.Search>
+            <Link to="/cart">
+              <ShoppingCartOutlined style={{ fontSize: 32, color: 'white' }} />
+            </Link>
+          </Flex>
+        </StyledHeaderSection>
+      </Container>
     </StyledHeader>
   );
 }
