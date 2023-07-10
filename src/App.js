2b41604617e0dce743220f70c9e988aa238a04c1
Peter Chen <peterchen2017125@gmail.com>
2023-07-10 14:55:15 +0800

    add login route

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import CheckoutPage from './pages/CheckoutPage';
 import ProductCollectionPage from './pages/ProductCollectionPage';
 import ProductPage from './pages/ProductPage';
 import ScrollToTop from './components/common/ScrollToTop';
+import { AuthProvider } from './components/auth/AuthContext';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path='/*' element={<Navigate to="/mall" />} />
-        <Route path='/mall' element={<HomePage />} exact />
-        <Route path='/cart' element={<CartPage />} exact />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route path='/mall/:categoryName' element={<ProductCollectionPage />} />
-        <Route path='/:productName' element={<ProductPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/*' element={<Navigate to="/mall" />} />
+          <Route path='/mall' element={<HomePage />} exact />
+          <Route path='/login' element={<LoginPage />} exact />
+          <Route path='/cart' element={<CartPage />} exact />
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/mall/:categoryName' element={<ProductCollectionPage />} />
+          <Route path='/:productName' element={<ProductPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
