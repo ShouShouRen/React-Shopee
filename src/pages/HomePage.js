865af8994fad6eb402e279549178e3cb8d91fbed
Peter Chen <peterchen2017125@gmail.com>
2023-07-10 11:37:29 +0800

    add Product Card

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,9 @@ import { Carousel } from "antd";
 import Banner1 from "../images/banner/banner1.jpg"
 import Banner2 from "../images/banner/banner2.jpg"
 import Banner3 from "../images/banner/banner3.jpg"
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product/ProductCards";
+import "../css/style.css";
 
 const BannerBox = styled.div`
   display: flex;
@@ -15,6 +18,7 @@ const BannerBox = styled.div`
 const BannerCarouselContainer = styled.div`
   width: 100%;
   padding: 1px;
+  box-sizing: border-box;
   @media(min-width: 769px){
     width: 66.333333%;
   }
@@ -22,6 +26,7 @@ const BannerCarouselContainer = styled.div`
 
 const BannerSectionContainer = styled.div`
   width: 100%;
+  box-sizing: border-box;
   padding: 1px;
   @media(min-width: 769px){
     width: 33.333333%;
@@ -33,7 +38,7 @@ const BannerContainer = styled.div`
   padding: 1px;
 `;
 
-const CarouselImage = styled.div`
+const Image = styled.div`
   width: 100%;
   height: ${(props) => props.height}px;
   background-size: cover;
@@ -42,6 +47,43 @@ const CarouselImage = styled.div`
   background-image: ${(props) => `url(${props.url})`};
 `;
 
+const BannerFeatureContainer = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: #fff;
+  padding: 8px 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1 );
+  border-right: 1px solid rgba(0, 0, 0, 0.1 );
+  border-left: 1px solid rgba(0, 0, 0, 0.1 );
+
+`;
+
+const CustomerContainer = styled.div`
+  margin-bottom: 48px;
+`;
+
+const ProductCollectionContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 48px;
+  `;
+const ProductContainer = styled.div`
+  width: 100%;
+  padding: 6px;
+  @media (min-width: 577px) {
+    width: calc(100% / 2 - 12px);
+  }
+  @media (min-width: 769px) {
+    width: calc(100% / 6 - 12px);
+  }
+`;
+
+const BannerFeatureColumn = styled.div`
+  width: calc(100% / 3);
+  text-align: center;
+`;
 
 const HomePage = () => {
   return (
@@ -50,20 +92,102 @@ const HomePage = () => {
       <BannerBox>
         <BannerCarouselContainer>
           <Carousel autoplay>
-            <CarouselImage height={200} url={Banner1}></CarouselImage>
-            <CarouselImage height={200} url={Banner2}></CarouselImage>
-            <CarouselImage height={200} url={Banner3}></CarouselImage>
+            <Link to="/p001">
+              <Image height={200} url={Banner1}></Image>
+            </Link>
+            <Link to="/p002">
+              <Image height={200} url={Banner2}></Image>
+            </Link>
+            <Link to="/p003">
+              <Image height={200} url={Banner3}></Image>
+            </Link>
           </Carousel>
         </BannerCarouselContainer>
         <BannerSectionContainer>
           <BannerContainer>
-            <CarouselImage height={100} url={Banner1}></CarouselImage>
+            <Link to="/p004">
+              <Image height={100} url={Banner1}></Image>
+            </Link>
           </BannerContainer>
           <BannerContainer>
-            <CarouselImage height={100} url={Banner1}></CarouselImage>
+            <Link to="/p005">
+              <Image height={100} url={Banner1}></Image>
+            </Link>
           </BannerContainer>
         </BannerSectionContainer>
+        <BannerFeatureContainer>
+          <BannerFeatureColumn>15天鑑賞期</BannerFeatureColumn>
+          <BannerFeatureColumn>退貨無負擔</BannerFeatureColumn>
+          <BannerFeatureColumn>假一賠二</BannerFeatureColumn>
+        </BannerFeatureContainer>
       </BannerBox>
+      <CustomerContainer>
+        <Link to="/p006">
+          <Image height={100} url={Banner1}></Image>
+        </Link>
+      </CustomerContainer>
+      <ProductCollectionContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo1" coverUrl={Banner1} salePrices={[100, 200]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo2" coverUrl={Banner2} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo3" coverUrl={Banner3} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo3" coverUrl={Banner3} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo2" coverUrl={Banner2} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo1" coverUrl={Banner1} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo1" coverUrl={Banner1} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo2" coverUrl={Banner2} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo3" coverUrl={Banner3} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo3" coverUrl={Banner3} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo2" coverUrl={Banner2} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+        <ProductContainer>
+          <Link to="p01" className="tdn">
+            <ProductCard title="foo1" coverUrl={Banner1} salePrices={[100, 200]} listPrices={[200, 299]}></ProductCard>
+          </Link>
+        </ProductContainer>
+      </ProductCollectionContainer>
     </DefaultLayout>
   )
 }
