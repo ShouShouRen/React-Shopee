7544ad66c8f699d011b24a48d4b617ed3d04e097
Peter Chen <peterchen2017125@gmail.com>
2023-07-10 11:37:12 +0800

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, ScrollRestoration } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import ProductCollectionPage from './pages/ProductCollectionPage';
 import ProductPage from './pages/ProductPage';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import ScrollToTop from './components/common/ScrollToTop';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
+      <ScrollToTop />
       <Routes>
         <Route path='/*' element={<Navigate to="/mall" />} />
         <Route path='/mall' element={<HomePage />} exact />
         <Route path='/cart' element={<CartPage />} exact />
-        <Route path='/checkout' element={<CheckoutPage exact />} />
+        <Route path='/checkout' element={<CheckoutPage />} />
         <Route path='/mall/:categoryName' element={<ProductCollectionPage />} />
         <Route path='/:productName' element={<ProductPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
